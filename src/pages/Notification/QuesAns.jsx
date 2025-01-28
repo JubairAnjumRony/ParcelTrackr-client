d7a62ed4f561075a3f31e68c15e5b345f571ef41
JubairAnjumRony <rony.ict.iu@gmail.com>
2025-01-28 18:27:50 +0600

    quesAns page added

--- src/pages/Notification/QuesAns.jsx
+++ src/pages/Notification/QuesAns.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const QuesAns = () => {
+
+    const questionsAndAnswers = [
+        {
+          question: 'What is React?',
+          answer: 'React is a JavaScript library for building user interfaces.',
+        },
+        {
+          question: 'What is Tailwind CSS?',
+          answer: 'Tailwind CSS is a utility-first CSS framework for creating custom designs directly in your HTML.',
+        },
+        {
+          question: 'What are components in React?',
+          answer: 'Components are reusable pieces of UI in React, which help in building complex interfaces.',
+        },
+      ];
     return (
         <div>
-            
+            <h2>this is new page section</h2>
         </div>
     );
 };
